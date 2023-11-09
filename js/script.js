23201c408dd8990ad171049f36be493e2d2c0c41
Jacob <spooner.jacob11@gmail.com>
2023-11-09 15:50:56 -0500

    WIP - improved design

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
+// <!-- typed js effect ends -->
